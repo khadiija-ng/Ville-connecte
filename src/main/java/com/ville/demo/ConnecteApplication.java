9ca42bb7eb5e3e1ab24d3de332a952e796e88768
Khadiija-ng <niangkhady171@gmail.com>
2025-12-29 21:08:40 +0000

    first commit

---
+++ src/main/java/com/ville/demo/ConnecteApplication.java
@@ -0,0 +1,13 @@
+package com.ville.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConnecteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConnecteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ville/demo/ConnecteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ville.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConnecteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
